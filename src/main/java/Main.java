cef45929dd63621cb89ad8bc0ddb134c20df3406
Sett311 <2004sett@gmail.com>
2023-06-23 23:34:03 +0300

    reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,21 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    // задаем ожидаемое значение
-    long expected = 30;
+        // задаем ожидаемое значение
+        long expected = 30;
 
-    // вызываем целевой метод
-    long actual = service.calculate(amount, registered);
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
 
-    // сравниваем ожидаемый и фактический результат
-    boolean result = expected == actual;
+        // сравниваем ожидаемый и фактический результат
+        boolean result = expected == actual;
 
-    // выводим результат
-    System.out.println(result);
-  }
+        // выводим результат
+        System.out.println(result);
+    }
 }
 
